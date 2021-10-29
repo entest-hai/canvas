9270661cef98717142321d493e01bcca2464a486
Tran Minh Hai <hai@bio-rithm.com>
2021-10-29 15:00:46 +0700

    test web worker

---
+++ typescript/script.ts
@@ -0,0 +1,7 @@
+function greeter(person: string) {
+    return "Hello, " + person
+}
+
+var user = "hai"
+
+console.log(greeter(user))
---
+++ web-worker/script.js
@@ -0,0 +1,30 @@
+// self.addEventListener('message', (ev) => {
+//     console.log('web worker started with data: ', ev.data)
+//     let data = ev.data
+//     var i = 0 
+//     switch (data) {
+//         case 'Get Started':
+//             // doing something here such as get/post request 
+//             self.postMessage('Web Worker Started')
+//             i = i + 1
+//             self.postMessage(i)
+//             break;
+//         case 'Other':
+//             self.postMessage('Other task ...')
+//             break;
+//         default:
+//             console.log("Invalid access")
+//             self.postMessage("Closing web worker")
+//             self.close()
+//     }
+// })
+
+var i = 0; 
+
+function timedCount() {
+    i = i + 1 
+    postMessage(i);
+    setTimeout("timedCount()", 500)
+}
+
+timedCount()
